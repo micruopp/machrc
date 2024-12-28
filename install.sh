7db2b3fb32a6474d9f359c436cf041368e221f47
Mic Ruopp <micruopp@gmail.com>
2024-12-27 21:21:58 -0600

    start to outline some steps

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
 echo "You didn't write the install script yet, dummy."
+
+# Installation
+#
+# 1. Package manager
+#   - Homebrew
+#   - MacPorts
+#
+# - asdf
+#
+# n. Link source files
+#   - .zshrc, .zlogin, .zlogout
+#   - .vimrc, <neovim folder>
+#   - .tool-versions
