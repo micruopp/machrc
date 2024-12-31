a6508b10971fe455b93cf2dee54655eedc5fa4d2
Mic Ruopp <micruopp@gmail.com>
2024-12-30 23:19:32 -0600

    "create git.sh"

---
+++ sh/git.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# git.sh
+# @desc Aliases and helpers for Git CLI
+
+gitac() {
+  echo "\"$@\""
+  #git add .
+  #git commit -m "$@"
+}
+
+gitcm() {
+  git commit -m "\"$1\""
+}
+
+gitlo() {
+  git log --oneline
+}
+
+gitst() {
+  git status
+}
+
