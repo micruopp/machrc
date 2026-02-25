51f6145ef0c21ae190d32949171bab7680dd4f14
Mic Ruopp <micruopp@gmail.com>
2026-02-24 22:29:38 -0600

    add comments for turning off os update notifications

---
+++ macos/setup.sh
@@ -0,0 +1,16 @@
+# source: https://discussions.apple.com/thread/255859390?sortBy=rank
+#
+# content:
+# damat-le
+# damat-le
+# User level: Level 1
+# 38 points
+# Feb 21, 2025 1:01 AM in response to thecoolwinter
+# 
+# You can try the following two terminal commands:
+# 
+# defaults write com.apple.SoftwareUpdate MajorOSUserNotificationDate -date "2030-02-07 23:22:47 +0000"
+# 
+# defaults write com.apple.SoftwareUpdate UserNotificationDate -date "2030-02-07 23:22:47 +0000"
+# 
+# The exact dates are not significant, as long as it's sometime in the future.
